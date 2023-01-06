4b30dc0f24ca9c2d01550f7040d8b7027c4baca8
Thaina <thschumann85@outlook.de>
2023-01-06 14:53:59 +0100

    jquery

--- js/script.js
+++ js/script.js
@@ -1,40 +1,34 @@
 function addTask(){
-    const newTask = document.getElementsByClassName('new-task')[0].value
-    if (newTask == '') {return}
-    const $ul = document.getElementsByTagName('ul')[0]
-    const $li = document.createElement('li')
-    const $div = document.createElement('div')
-    $div.className += 'checkbox'
-    const $label = document.createElement('label')
-    const $input = document.createElement('input')
-    $input.className += 'task-item'
-    $input.setAttribute('type', 'checkbox')
-    $input.setAttribute('name', 'tarefa')
-    $input.addEventListener('click', toggleRemoved)
-    $label.appendChild($input)
-    $label.appendChild(document.createTextNode(newTask))
-    $div.appendChild($label)
-    $li.appendChild($div)
-    $ul.appendChild($li)
-    document.getElementsByClassName('new-task')[0].value = ''
+    const newTask = $('.new-task').val()
+    if (newTask === '') { return }
+    const $ul = $('ul')
+    const $li = $('<li>').appendTo($ul)
+    const $div = $('<div>').addClass('checkbox').appendTo($li)
+    const $label = $('<label>').appendTo($div)
+    $('<input>')
+       .attr('type', 'checkbox')
+       .attr('name', 'task')
+       .addClass('task-item')
+       .click(toggleRemoved)
+       .appendTo($label)
+    $label.append(newTask)
+
+    
+    $('.new-task').val('')
 }
 
 
 function toggleRemoved(e) {
-    const $li = e.currentTarget.closest('li')
-    $li.classList.toggle('removed')
+    const $li = $(e.currentTarget).closest('li')
+    $li.toggleClass('removed')
 
 }
 
 function changeTitle(){
-    const title = document.getElementsByTagName('h2')[0]
-    const newTitle = document.getElementsByClassName('change-title')[0].value
-    title.innerHTML = newTitle
+    const newTitle = $('.change-title').val()
+    $('h2').text(newTitle)
 }
 
-document.getElementsByClassName('add-task')[0].addEventListener('click', addTask)
-
-const tasks = document.getElementsByClassName('task-item')
-Array.from(tasks).forEach(task => task.addEventListener('click',toggleRemoved))
-
-document.getElementsByClassName('change-title')[0].addEventListener('keyup', changeTitle) 
+$('.add-task').click(addTask)
+$('.task-item').click(toggleRemoved) 
+$('.change-title').keyup(changeTitle) 
